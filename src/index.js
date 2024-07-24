077fae06c4c2012f44f581d0b9738c3dd46441b9
jesusignazio <jesusignazio@gmail.com>
2024-07-24 21:26:27 +0200

    first commit2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,40 @@
 import _ from 'lodash';
 
-function component() {
-  const element = document.createElement('div');
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  return element;
+const yahooFinance = require('yahoo-finance');
+
+// Function to get the current share price
+function getCurrentSharePrice(tickerSymbol) {
+  return new Promise((resolve, reject) => {
+    yahooFinance.quote({
+      symbol: tickerSymbol,
+      modules: ['price']
+    }, (err, quotes) => {
+      if (err) {
+        reject(err);
+      } else {
+        const price = quotes.price.regularMarketPrice;
+        resolve(Math.round(price)); // Convert to integer format
+      }
+    });
+  });
 }
 
-document.getElementById('root').appendChild(component());
+// Function to get the shares outstanding
+function getSharesOutstanding(tickerSymbol) {
+  return new Promise((resolve, reject) => {
+    yahooFinance.quote({
+      symbol: tickerSymbol,
+      modules: ['defaultKeyStatistics']
+    }, (err, quotes) => {
+      if (err) {
+        reject(err);
+      } else {
+        const sharesOutstanding = quotes.defaultKeyStatistics.sharesOutstanding;
+        resolve(sharesOutstanding); // Return as integer
+      }
+    });
+  });
+}
+
+// Example usage
+const tickerSymbol = 'ZIM';
